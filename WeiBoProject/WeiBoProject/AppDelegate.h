4e90d0498d49f26e55a689de64c10b6188a1ce24
黄志强 <hantianit@outlook.com>
2015-11-05 22:21:32 +0800

    创建新项目

---
+++ WeiBoProject/WeiBoProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiBoProject
+//
+//  Created by Hanten on 15/11/5.
+//  Copyright © 2015年 Hanten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeiBoProject/WeiBoProject/ViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeiBoProject
+//
+//  Created by Hanten on 15/11/5.
+//  Copyright © 2015年 Hanten. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
