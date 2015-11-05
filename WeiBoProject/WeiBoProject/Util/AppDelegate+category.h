41bbe4bd000a9fabc1e28cca957c455649ba428c
黄志强 <hantianit@outlook.com>
2015-11-05 22:48:14 +0800

    用cocoaPod pod 进常用的第三方框架

---
+++ WeiBoProject/WeiBoProject/Util/AppDelegate+category.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+category.h
+//  WeiBoProject
+//
+//  Created by Hanten on 15/11/5.
+//  Copyright © 2015年 Hanten. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (category)
+
+@end
